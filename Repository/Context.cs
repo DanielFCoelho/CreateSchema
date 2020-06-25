94fe8cee9d8f1cfe6504e8881d9dad266c80decf
DanielFCoelho <daniel_freita@outlook.com>
2020-06-25 10:59:58 -0300

    Add files via upload

---
+++ Repository/Context.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Repository
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=DB_TESTE;Uid=root;Pwd=root1234;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            EntityTypeBuilder<Produto> builder = modelBuilder.Entity<Produto>();
+            builder.ToTable("Produto");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).HasColumnName("Id").IsRequired();
+            builder.Property(p => p.Nome).HasColumnName("Nome").IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
