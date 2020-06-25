14bfcf7bcc0b705d3aaac2728c8715bcc841861b
DanielFCoelho <daniel_freita@outlook.com>
2020-06-25 10:38:01 -0300

    Add files via upload

---
+++ API/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var ctx = new Context();
+            var teste = ctx.Set<Produto>().ToList();
+            return null;
+        }
+    }
+}
