f7953536d66438a4b6802dcdc9179bbceacfcd19
DanielFCoelho <daniel_freita@outlook.com>
2020-06-25 10:55:57 -0300

    Add files via upload

---
+++ Domain/Produto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string TesteAtualizacao { get; set; }
+    }
+}
